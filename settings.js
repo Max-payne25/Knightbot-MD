8e9ce4c8d2c88ac2ebb1a23aedebfd1ebb13db61
Max-payne25 <maxpay2525@gmail.com>
2025-06-11 23:24:51 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'knight Bot', // Your name
-  ownerNumber: '254721503256', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+254721503256', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
