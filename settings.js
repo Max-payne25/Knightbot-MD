515a96d38e87632b99b0be84b3ff46c77a46ace1
Max-payne25 <maxpay2525@gmail.com>
2026-07-03 22:39:37 +0300

    Change command mode from public to private

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   botOwner: 'knight bot', // Your name
   ownerNumber: '254721503256', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.7",
 };
